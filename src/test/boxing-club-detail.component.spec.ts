82b2394a6c9c2e6804cd3ca14c6f30851a43ca74
omaroz46 <omar.ozan@bbzbl-it.ch>
2024-05-31 14:48:54 +0200

    lint

--- src/test/boxing-club-detail.component.spec.ts
+++ src/test/boxing-club-detail.component.spec.ts
@@ -1,5 +1,4 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importiere BrowserAnimationsModule
-import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
